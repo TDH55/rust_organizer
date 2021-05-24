88190e60c2435ed2dccf474869f823376799fa7a
TDH55 <taylorhoward1@wustl.edu>
2021-05-24 17:17:56 -0500

    Setup structopt parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Cli {
+    #[structopt(short = "o", long = "origin", default_value = "./", parse(from_os_str))]
+    root: std::path::PathBuf,
+    #[structopt(short = "d", long = "destination", default_value = "Rust_Organizer", parse(from_os_str))] //TODO: change this to include the current date
+    destination: std::path::PathBuf,
+    #[structopt(short = "e", long = "extension")]
+    extension: String,
+    #[structopt(short = "m", long = "move")]
+    move_files: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    println!("Args: {:?}", args);
 }
